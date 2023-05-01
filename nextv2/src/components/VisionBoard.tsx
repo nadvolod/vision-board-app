a82b38870edf1871311a9925bd82cc9474cd06d6
nadvolod <nadvolod@gmail.com>
2023-04-30 17:44:29 -0700

    trying to add a working close button

--- nextv2/src/components/VisionBoard.tsx
+++ nextv2/src/components/VisionBoard.tsx
@@ -1,5 +1,6 @@
 // components/VisionBoard.tsx
 import React, { useState } from 'react';
+import Modal from 'react-responsive-modal';
 
 interface Goal {
   id: number;
@@ -67,23 +68,68 @@ const initialGoals: Goal[] = [
 
 const VisionBoard: React.FC = () => {
   const [goals] = useState(initialGoals);
+  const [open, setOpen] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const onOpenModal = (index: number) => {
+    setCurrentIndex(index);
+    setOpen(true);
+  };
+
+  const onCloseModal = () => {
+    setOpen(false);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((currentIndex - 1 + goals.length) % goals.length);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((currentIndex + 1) % goals.length);
+  };
 
   return (
     <div className="grid grid-cols-3 gap-4 h-screen w-screen p-4 bg-gradient-to-br from-indigo-400 via-purple-400 to-pink-300">
-      {goals.map((goal) => (
+      {goals.map((goal, index) => (
         <div
           key={goal.id}
           className="border-2 border-indigo-200 rounded-lg p-4 h-full w-full flex flex-col justify-between bg-white bg-opacity-90 shadow-xl"
+          onClick={() => onOpenModal(index)}
         >
           <h3 className="text-lg font-bold mb-2 text-indigo-800">{goal.description}</h3>
           <p className="text-indigo-600">Due date: {goal.dueDate}</p>
           <img
             src={goal.imageUrl}
             alt="Goal"
-            className="mt-2 w-full h-auto flex-grow rounded-lg object-cover"
+            className="mt-2 w-full h-auto flex-grow rounded-lg object-cover cursor-pointer"
           />
         </div>
       ))}
+      <Modal open={open} onClose={onCloseModal} center>
+        <div className="w-screen h-screen flex flex-col justify-between">
+          <img src={goals[currentIndex].imageUrl} alt="Goal" className="w-full h-auto mt-2" />
+          <div className="flex justify-between mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handlePrev}
+            >
+              Previous
+            </button>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+          <button
+            className="absolute top-4 right-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={onCloseModal}
+          >
+            Close
+          </button>
+        </div>
+      </Modal>
     </div>
   );
 };
